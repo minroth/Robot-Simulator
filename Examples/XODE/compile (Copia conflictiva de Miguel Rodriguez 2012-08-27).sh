7a2ab5bef35de4a48f1fce8e3f6f373f0a393946
Miguel Rodriguez <miguel.andres.rodriguez.s@gmail.com>
2014-01-04 00:15:51 -0300

    More Clean
    Más limpieza al repositorio. Esta vez archivos de configuración y
    archivos temporales que no debieron haberse subido en un comienzo.

---
+++ Examples/XODE/compile (Copia conflictiva de Miguel Rodriguez 2012-08-27).sh
@@ -0,0 +1,2 @@
+g++ -o Standar_code Standar_code.cpp -I/home/minroth/ODE/Engine/include/ /home/minroth/ODE/Engine/ode/src/.libs/libode.a /home/minroth/ODE/Engine/drawstuff/src/.libs/libdrawstuff.a -lGL -lX11 -lGLU -ltinyxml
+
